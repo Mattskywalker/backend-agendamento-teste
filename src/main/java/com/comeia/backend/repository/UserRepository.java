02d0430e5d45978c07c7248e4284664d78d65ee3
Mateus J. Martins <mateus.lima@partners.mv.com.br>
2022-03-30 19:22:56 -0300

    user repository

--- src/main/java/com/comeia/backend/repository/UserRepository.java
+++ src/main/java/com/comeia/backend/repository/UserRepository.java
@@ -9,4 +9,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     public Optional<User> findByEmail(String login);
+
+    public Optional<User> findByCpf(String cpf);
 }
